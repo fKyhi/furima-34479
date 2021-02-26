b88d3ff0ee20aa1cfff948ec010bd09b72d2a13f
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-02-26 17:58:14 +0900

    販売手数料、利益計算のJavascript実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+  
+  const addTaxDom = document.getElementById("add-tax-price")
+  const profitPrice = document.getElementById("profit")
+  const Tax = 0.1;
+  const Tax_fee = inputValue * Tax;
+  const Profit = inputValue - Tax_fee;
+  addTaxDom.innerHTML =  Tax_fee;
+  profitPrice.innerHTML = Profit;
+
+  })
+})
