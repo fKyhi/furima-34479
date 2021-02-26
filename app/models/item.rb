fc2edbbeda51b04049141b05609c48ba759e87fb
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-02-26 16:33:19 +0900

    テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,9 +11,11 @@ class Item < ApplicationRecord
 
 
   with_options presence: true do
+    validates :image
     validates :name
     validates :explain
     validates :price, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999 }
+    validates :price, format: {with: /\A[0-9]+\z/}
     
     with_options numericality: { other_than: 1 } do
       validates :category_id
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,19 @@
 FactoryBot.define do
   factory :item do
-    image {Faker::Lorem.sentence}
-    name {'aaa'}
-    explain {'aaa'}
-    category_id { Category.all.sample }
-    state_id { State.all.sample }
-    shipping_fee_status_id { Shipping_fee_status.all.sample }
-    prefecture_id { Prefecture.all.sample }
-    scheduled_delivery_id { Scheduled_delivery.all.sample }
-    price { }
+    name                   {'aaa'}
+    explain                {'aaa'}
+    category_id            { 2 }
+    state_id               { 2 }
+    shipping_fee_status_id { 2 }
+    prefecture_id          { 2 }
+    scheduled_delivery_id  { 2 }
+    price                  { 5000 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
 
   end
 end
