5c692f0dac3ed9847b9aaf9de0b8823e602d78da
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-02-26 11:59:52 +0900

    商品出品機能実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    image {Faker::Lorem.sentence}
+    name {'aaa'}
+    explain {'aaa'}
+    category_id { Category.all.sample }
+    state_id { State.all.sample }
+    shipping_fee_status_id { Shipping_fee_status.all.sample }
+    prefecture_id { Prefecture.all.sample }
+    scheduled_delivery_id { Scheduled_delivery.all.sample }
+    price { }
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  before do
+    @item = FactoryBot.build(:item)
+
+    describe '商品出品登録' do
+      context '商品出品できるとき' do
+        it
+      end
+      context '商品出品できないとき' do
+        
+      end
+    end
+  end
