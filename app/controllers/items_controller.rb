534a03b4d81c63361ccd8c72e792a5960fd4ee3e
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-03-02 15:18:53 +0900

    修正1(可読性向上、コード修正)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
-  before_action :move_to_new, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -21,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -44,12 +41,12 @@ class ItemsController < ApplicationController
                                  :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
   end
 
-  def move_to_new
-    redirect_to new_user_session_path unless user_signed_in?
-  end
-
   def move_to_index
     item = Item.find(params[:id])
     redirect_to action: :index unless current_user.id == item.user_id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
