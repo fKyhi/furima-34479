f36f9879046c3ecee0773e1e4b3f3d87f268fc74
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-03-02 14:44:34 +0900

    商品情報編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_new, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @items = Item.order('created_at DESC')
@@ -22,10 +24,36 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
-  def item_params
-    params.require(:item).permit(:name, :explain, :category_id, :state_id, :shipping_fee_status_id, :prefecture_id,
-                                 :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
-  end
+    def item_params
+      params.require(:item).permit(:name, :explain, :category_id, :state_id, :shipping_fee_status_id, :prefecture_id,
+                                  :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
+    end
+
+    def move_to_new
+      unless user_signed_in?
+        redirect_to new_user_session_path
+      end
+    end
+
+    def move_to_index
+      item = Item.find(params[:id])
+      unless current_user.id == item.user_id
+        redirect_to action: :index
+      end
+    end
 end
