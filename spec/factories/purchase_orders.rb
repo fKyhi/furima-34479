a0b14514807aeab48ff39ae03dc9eec7a2ad139a
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-03-05 14:47:55 +0900

    テストコード実装

--- spec/factories/purchase_orders.rb
+++ spec/factories/purchase_orders.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :purchase_order do
-    
+    post_code       { "111-1111" }
+    prefecture_id   { 2 }
+    city            {"兵庫県"}
+    addressed       {"神戸市"}
+    building        {"マンション"}
+    phone_number    {"00000000000"}
   end
 end
--- spec/models/purchase_order_spec.rb
+++ spec/models/purchase_order_spec.rb
@@ -1,5 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品購入機能' do
+    before do
+      @purchase_order = FactoryBot.build(:purchase_order)
+    end
+
+    context '商品購入できたとき' do
+      it 'post_code、prefecture_id、city、addressed、building、phone_numberが存在すれば購入できる' do
+        expect(@purchase_order).to be_valid
+      end
+      it 'buildingが存在しなくても購入できる' do
+        @purchase_order.building = ' '
+        expect(@purchase_order).to be_valid
+      end
+    end
+    context '商品購入できないとき' do
+      it 'post_codeが空だと購入できない' do
+        @purchase_order.post_code = ' '
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include("Post code can't be blank", "Post code is invalid. Include hyphen(-)")
+      end
+      it 'post_codeに[-]が存在しないとき購入できない' do
+        @purchase_order.post_code = '1111111'
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include ("Post code is invalid. Include hyphen(-)")
+      end
+      it 'prefecture_idが1のとき購入できない' do
+        @purchase_order.prefecture_id = 1
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include ("Prefecture can't be blank")
+      end
+      it 'cityが存在しないとき購入できない' do
+        @purchase_order.city = ' '
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include ("City can't be blank")
+      end
+      it 'addressedが存在しないとき購入できない' do
+        @purchase_order.addressed = ' '
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include ("Addressed can't be blank")
+      end
+      it 'phone_numberが存在しないとき購入できない' do
+        @purchase_order.phone_number = ' '
+        @purchase_order.valid?
+        expect(@purchase_order.errors.full_messages).to include ("Phone number can't be blank")
+      end
+    end
+  end
 end
