4e7785ec66d94b59d304be8803afac02c644909e
fKyhi <78200228+fKyhi@users.noreply.github.com>
2021-03-09 17:09:24 +0900

    エラーコード重複修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :explain
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-    validates :price, format: { with: /\A[0-9]+\z/ }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
